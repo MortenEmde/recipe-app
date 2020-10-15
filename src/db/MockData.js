6764805de0f6e78b11bb1fbc128624e37da1600a
Emde <mortenemdec@gmail.com>
2020-10-15 13:18:55 +0200

    add categories to MockData.js

--- src/db/MockData.js
+++ src/db/MockData.js
@@ -1,6 +1,38 @@
+export const categories = [
+  {
+    id: 1,
+    name: 'Italian Food',
+    photo_url:
+    'https://forkfeeds.com/wp-content/uploads/2019/08/Italian-food-1.jpeg'
+  },
+  {
+    id: 2,
+    name: 'Mexican Food',
+    photo_url: 'https://img1.mashed.com/img/gallery/mexican-foods-you-need-to-try-before-you-die/intro-1585677665.jpg'
+  },
+  {
+    id: 3,
+    name: 'Asian Food',
+    photo_url:
+      'https://japansenoodles.nl/wp-content/uploads/2017/07/585be1aa1600002400bdf2a6.jpeg'
+  },
+  {
+    id: 4,
+    name: 'American Food',
+    photo_url:
+    'https://previews.123rf.com/images/baibakova/baibakova1807/baibakova180700091/105083577-american-food-fast-food-top-view.jpg'
+  },
+  {
+    id: 5,
+    name: 'Desserts',
+    photo_url: 'https://i0.wp.com/www.eatthis.com/wp-content/uploads/2019/07/dessert-table.jpg?fit=1200%2C879&ssl=1'
+  },
+];
+
 export const recipes = [
   {
     recipeId: 10,
+    categoryId: 1,
     title: 'Spaghetti Meatballs',
     photoUrl: 'https://www.irenemilito.it/wp-content/uploads/2019/12/1506456062-delish-spaghetti-meatballs.jpg',
     time: '60',
@@ -15,6 +47,7 @@ export const recipes = [
   },
   {
     recipeId: 11,
+    categoryId: 3,
     title: 'Chicken Katsu',
     photoUrl: 'https://images.japancentre.com/recipes/pics/301/main/301_Chicken_Katsu_Curry.jpg?1469573012',
     time: '30',
@@ -30,49 +63,55 @@ export const recipes = [
   },
   {
     recipeId: 12,
+    categoryId: 3,
     title: 'Butter Chicken',
     photoUrl: 'https://easychickenrecipes.com/wp-content/uploads/2019/11/butter-chicken-reshoot-6-of-8-500x500.jpg',
     time: '60',
     ingredients: [
-      ['Meat', '500g'],
-      ['Tomato Passata', '750ml'],
-      ['Oregano', '1tbsp'],
-      ['Spaghetti', '100g'],
-    ],
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lacinia finibus magna, consectetur molestie enim venenatis eu. Donec felis magna, lacinia eu nunc sit amet, ultricies malesuada lacus. Nunc urna orci, tempus eget ullamcorper in, tempor vitae elit. Cras ut interdum sapien. Maecenas dictum nunc at nunc auctor condimentum. In nunc ipsum, mattis ac nulla at, porttitor pulvinar massa. Sed lacinia velit eu justo elementum euismod. Mauris turpis eros, congue sed arcu at, volutpat fringilla justo.\n\n Ut aliquet urna a risus gravida fringilla. Nullam quis purus dui. Phasellus congue pharetra enim vel volutpat. Sed accumsan diam id cursus euismod. In hac habitasse platea dictumst. Duis maximus eros nulla, sit amet porttitor quam luctus et. Fusce condimentum sagittis magna, sit amet molestie mi ullamcorper in. Praesent dapibus dolor non turpis vestibulum volutpat quis et tortor. Sed id enim massa. Cras vel dignissim ligula. Ut imperdiet leo elit, sed suscipit quam mattis vel. Mauris vulputate quam massa, nec tincidunt erat volutpat quis. Nullam quis lorem magna. Nulla sed vulputate nunc. Curabitur scelerisque, nisl ac vehicula ultricies, metus mauris maximus tortor, eu scelerisque lectus nibh vel lorem. Curabitur faucibus nibh sed sapien hendrerit laoreet.\n\n Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris pulvinar posuere nulla a ultrices. Vivamus dui erat, porttitor vitae laoreet quis, ultrices non metus. Quisque ut imperdiet dolor. Maecenas sit amet mauris porta, ullamcorper ipsum quis, faucibus arcu. Vestibulum quam orci, eleifend ac tortor tristique, facilisis fermentum dui. Donec tristique, ligula vitae elementum iaculis, magna metus vulputate sem, in ultricies justo urna sed sem. Nam ac justo ac augue luctus vulputate nec vel magna. Etiam id leo fringilla, egestas est a, tincidunt risus.'
-  },
-  {
-    recipeId: 13,
-    title: 'Chicken Pesto Pasta',
-    photoUrl: 'https://www.thewholesomedish.com/wp-content/uploads/2015/04/Chicken-Pesto-Pasta-016.jpg',
-    time: '30',
-    ingredients: [
       ['Chicken', '500g'],
-      ['Onion', '2pc'],
-      ['Carrots', '500g'],
-      ['Curry Spice', '1tbsp'],
+      ['Tomato', '3pc'],
+      ['Oregano', '1tbsp'],
       ['Rice', '1cup'],
     ],
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lacinia finibus magna, consectetur molestie enim venenatis eu. Donec felis magna, lacinia eu nunc sit amet, ultricies malesuada lacus. Nunc urna orci, tempus eget ullamcorper in, tempor vitae elit. Cras ut interdum sapien. Maecenas dictum nunc at nunc auctor condimentum. In nunc ipsum, mattis ac nulla at, porttitor pulvinar massa. Sed lacinia velit eu justo elementum euismod. Mauris turpis eros, congue sed arcu at, volutpat fringilla justo.\n\n Ut aliquet urna a risus gravida fringilla. Nullam quis purus dui. Phasellus congue pharetra enim vel volutpat. Sed accumsan diam id cursus euismod. In hac habitasse platea dictumst. Duis maximus eros nulla, sit amet porttitor quam luctus et. Fusce condimentum sagittis magna, sit amet molestie mi ullamcorper in. Praesent dapibus dolor non turpis vestibulum volutpat quis et tortor. Sed id enim massa. Cras vel dignissim ligula. Ut imperdiet leo elit, sed suscipit quam mattis vel. Mauris vulputate quam massa, nec tincidunt erat volutpat quis. Nullam quis lorem magna. Nulla sed vulputate nunc. Curabitur scelerisque, nisl ac vehicula ultricies, metus mauris maximus tortor, eu scelerisque lectus nibh vel lorem. Curabitur faucibus nibh sed sapien hendrerit laoreet.\n\n Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris pulvinar posuere nulla a ultrices. Vivamus dui erat, porttitor vitae laoreet quis, ultrices non metus. Quisque ut imperdiet dolor. Maecenas sit amet mauris porta, ullamcorper ipsum quis, faucibus arcu. Vestibulum quam orci, eleifend ac tortor tristique, facilisis fermentum dui. Donec tristique, ligula vitae elementum iaculis, magna metus vulputate sem, in ultricies justo urna sed sem. Nam ac justo ac augue luctus vulputate nec vel magna. Etiam id leo fringilla, egestas est a, tincidunt risus.'
   },
   {
+    recipeId: 13,
+    categoryId: 1,
+    title: 'Chicken Pesto Pasta',
+    photoUrl: 'https://www.thewholesomedish.com/wp-content/uploads/2015/04/Chicken-Pesto-Pasta-016.jpg',
+    time: '30',
+    ingredients: [
+      ['Chicken', '500g'],
+      ['Basil', '50g'],
+      ['Spinach', '100g'],
+      ['Olive Oil', '1cup'],
+      ['Garlic', '2cloves'],
+      ['Pasta', '200g'],
+    ],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lacinia finibus magna, consectetur molestie enim venenatis eu. Donec felis magna, lacinia eu nunc sit amet, ultricies malesuada lacus. Nunc urna orci, tempus eget ullamcorper in, tempor vitae elit. Cras ut interdum sapien. Maecenas dictum nunc at nunc auctor condimentum. In nunc ipsum, mattis ac nulla at, porttitor pulvinar massa. Sed lacinia velit eu justo elementum euismod. Mauris turpis eros, congue sed arcu at, volutpat fringilla justo.\n\n Ut aliquet urna a risus gravida fringilla. Nullam quis purus dui. Phasellus congue pharetra enim vel volutpat. Sed accumsan diam id cursus euismod. In hac habitasse platea dictumst. Duis maximus eros nulla, sit amet porttitor quam luctus et. Fusce condimentum sagittis magna, sit amet molestie mi ullamcorper in. Praesent dapibus dolor non turpis vestibulum volutpat quis et tortor. Sed id enim massa. Cras vel dignissim ligula. Ut imperdiet leo elit, sed suscipit quam mattis vel. Mauris vulputate quam massa, nec tincidunt erat volutpat quis. Nullam quis lorem magna. Nulla sed vulputate nunc. Curabitur scelerisque, nisl ac vehicula ultricies, metus mauris maximus tortor, eu scelerisque lectus nibh vel lorem. Curabitur faucibus nibh sed sapien hendrerit laoreet.\n\n Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris pulvinar posuere nulla a ultrices. Vivamus dui erat, porttitor vitae laoreet quis, ultrices non metus. Quisque ut imperdiet dolor. Maecenas sit amet mauris porta, ullamcorper ipsum quis, faucibus arcu. Vestibulum quam orci, eleifend ac tortor tristique, facilisis fermentum dui. Donec tristique, ligula vitae elementum iaculis, magna metus vulputate sem, in ultricies justo urna sed sem. Nam ac justo ac augue luctus vulputate nec vel magna. Etiam id leo fringilla, egestas est a, tincidunt risus.'
+  },
+  {
     recipeId: 14,
+    categoryId: 2,
     title: 'Chicken Fajitas',
     photoUrl: 'https://www.cookingclassy.com/wp-content/uploads/2020/03/chicken-fajitas-18.jpg',
     time: '60',
     ingredients: [
-      ['Meat', '500g'],
-      ['Tomato Passata', '750ml'],
-      ['Oregano', '1tbsp'],
-      ['Spaghetti', '100g'],
+      ['Chicken', '500g'],
+      ['Paprica', '2pc'],
+      ['Tortilla', '4pc'],
+      ['Corn', '400g'],
+      ['Fajitas Spice', '1tbsp'],
     ],
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lacinia finibus magna, consectetur molestie enim venenatis eu. Donec felis magna, lacinia eu nunc sit amet, ultricies malesuada lacus. Nunc urna orci, tempus eget ullamcorper in, tempor vitae elit. Cras ut interdum sapien. Maecenas dictum nunc at nunc auctor condimentum. In nunc ipsum, mattis ac nulla at, porttitor pulvinar massa. Sed lacinia velit eu justo elementum euismod. Mauris turpis eros, congue sed arcu at, volutpat fringilla justo.\n\n Ut aliquet urna a risus gravida fringilla. Nullam quis purus dui. Phasellus congue pharetra enim vel volutpat. Sed accumsan diam id cursus euismod. In hac habitasse platea dictumst. Duis maximus eros nulla, sit amet porttitor quam luctus et. Fusce condimentum sagittis magna, sit amet molestie mi ullamcorper in. Praesent dapibus dolor non turpis vestibulum volutpat quis et tortor. Sed id enim massa. Cras vel dignissim ligula. Ut imperdiet leo elit, sed suscipit quam mattis vel. Mauris vulputate quam massa, nec tincidunt erat volutpat quis. Nullam quis lorem magna. Nulla sed vulputate nunc. Curabitur scelerisque, nisl ac vehicula ultricies, metus mauris maximus tortor, eu scelerisque lectus nibh vel lorem. Curabitur faucibus nibh sed sapien hendrerit laoreet.\n\n Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris pulvinar posuere nulla a ultrices. Vivamus dui erat, porttitor vitae laoreet quis, ultrices non metus. Quisque ut imperdiet dolor. Maecenas sit amet mauris porta, ullamcorper ipsum quis, faucibus arcu. Vestibulum quam orci, eleifend ac tortor tristique, facilisis fermentum dui. Donec tristique, ligula vitae elementum iaculis, magna metus vulputate sem, in ultricies justo urna sed sem. Nam ac justo ac augue luctus vulputate nec vel magna. Etiam id leo fringilla, egestas est a, tincidunt risus.'
   },
   {
     recipeId: 15,
+    categoryId: 5,
     title: 'Chocolate chip cookies',
     photoUrl: 'https://t8x8a5p2.stackpathcdn.com/wp-content/uploads/2020/06/Chocolate-Chip-Cookies-Recipe-720x720.jpg',
     time: '30',
@@ -88,6 +127,7 @@ export const recipes = [
   },
   {
     recipeId: 16,
+    categoryId: 4,
     title: 'Burger and fries',
     photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT9FkDlA6UeSfwyPGgpTarIgoFPEVZWDXuP4g&usqp=CAU',
     time: '30',
@@ -103,6 +143,7 @@ export const recipes = [
   },
   {
     recipeId: 17,
+    categoryId: 3,
     title: 'Bahmi',
     photoUrl: 'https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/24/03/80/picwqsOVR.jpg',
     time: '30',
@@ -118,6 +159,7 @@ export const recipes = [
   },
   {
     recipeId: 18,
+    categoryId: 1,
     title: 'Pizza',
     photoUrl: 'https://www.moulinex-me.com/medias/?context=bWFzdGVyfHJvb3R8MTQzNTExfGltYWdlL2pwZWd8aDM2L2g1Mi8xMzA5NzI3MzI2MjExMC5qcGd8N2MxZDhmNmI5ZTgzZDZlZWQyZGQ4YjFlZjUyNDlkMTExYjdkZDdlZmFkY2I0N2NmNjljOGViNmExZjIyMDU4Yw',
     time: '30',
